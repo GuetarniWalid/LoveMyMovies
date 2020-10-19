1d4db4360ebfc86d1c8763c3cd5da8532f93077c
walid guetarni <guetarni.walid@gmail.com>
2020-10-19 09:53:30 +0200

    style général page d'accueil terminé, non responsive

--- src/views/Accueil/Accueil.js
+++ src/views/Accueil/Accueil.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react'
 import Navbar from './Navbar/Navbar'
 import Slider from './Slider/Slider'
 import BottomNavbar from './BottomNavbar/BottomNavbar'
+import styles from './Slider/Slider.module.css'
 
 export default function Accueil() {
     return (
         <Fragment>
+            <div className={styles.bodyBackgroundCover}></div>
             <Navbar />
             <Slider />
             <BottomNavbar />
